09ea1d2517aaee9450698bfbbf62fed28c7e677c
Fryland <rylanddev@gmail.com>
2017-12-12 14:56:06 -0800

    The lazyfoo formatting BUGS THE FUUUUUCCCCCKKK OUT OF MEgit statusgit status Had to remove it all, and removed all of the error checking out of frustration and impatience, too. I'll add back the error checking later.

--- main2.c
+++ main2.c
@@ -22,218 +22,121 @@ SDL_Renderer* gRenderer = NULL;
 //Scene sprites
 SDL_Rect sprites[4];
 
-bool loadSheetFromFile(char path[256])
+void loadSheetFromFile(char path[256])
 {
-	//Load image at specified path
-	SDL_Surface* loadedSurface = IMG_Load(path);
-	if( loadedSurface == NULL )
-	{
-		printf( "Unable to load image %s! SDL_image Error: %s\n", path, IMG_GetError() );
-	}
-	else
-	{
-		//Color key image
-		SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
-
-		//Create texture from surface pixels
-        spritesheet = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
-		if( spritesheet == NULL )
-		{
-			printf( "Unable to create texture from %s! SDL Error: %s\n", path, SDL_GetError() );
-		}
-
-		//Get rid of old loaded surface
-		SDL_FreeSurface( loadedSurface );
-	}
-    return 1;
+    SDL_Surface* loadedSurface = IMG_Load(path);
+    SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF ));
+    spritesheet = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+    SDL_FreeSurface(loadedSurface);
 }
 
 void freeSheet()
 {
-	if(spritesheet != NULL )
-	{
-		SDL_DestroyTexture(spritesheet);
-		spritesheet = NULL;
-	}
+    SDL_DestroyTexture(spritesheet);
+    spritesheet = NULL;
 }
 
-void renderSprite( int x, int y, SDL_Rect* clip )
+void renderSprite(int x, int y, SDL_Rect* clip)
 {
-	//Set rendering space and render to screen
-	SDL_Rect renderQuad = { x, y, 16, 16};
-
-	//Set clip rendering dimensions
-	if( clip != NULL )
-	{
-		renderQuad.w = clip->w;
-		renderQuad.h = clip->h;
-	}
-
-	//Render to screen
-	SDL_RenderCopy( gRenderer, spritesheet, clip, &renderQuad );
+    SDL_Rect renderQuad = {x, y, 16, 16};
+    renderQuad.w = clip->w;
+    renderQuad.h = clip->h;
+    SDL_RenderCopy( gRenderer, spritesheet, clip, &renderQuad );
 }
 
 bool init()
 {
-	//Initialization flag
-	bool success = true;
-
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
-		success = false;
-	}
-	else
-	{
-		//Set texture filtering to linear
-		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
-		{
-			printf( "Warning: Linear texture filtering not enabled!" );
-		}
-
-		//Create window
-		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
-		{
-			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
-			success = false;
-		}
-		else
-		{
-			//Create renderer for window
-			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
-			if( gRenderer == NULL )
-			{
-				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
-				success = false;
-			}
-			else
-			{
-				//Initialize renderer color
-				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-
-				//Initialize PNG loading
-				int imgFlags = IMG_INIT_PNG;
-				if( !( IMG_Init( imgFlags ) & imgFlags ) )
-				{
-					printf( "SDL_image could not initialize! SDL_mage Error: %s\n", IMG_GetError() );
-					success = false;
-				}
-			}
-		}
-	}
-
-	return success;
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
+    gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+    SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    int imgFlags = IMG_INIT_PNG;
+    IMG_Init(imgFlags);
 }
 
 bool loadMedia()
 {
-	//Loading success flag
-	bool success = true;
+    loadSheetFromFile("kinknew.png");
 
-	//Load sprite sheet texture
-	if( !loadSheetFromFile( "kinknew.png" ) )
-	{
-		printf( "Failed to load sprite sheet texture!\n" );
-		success = false;
-	}
-	else
-	{
-		//Set top left sprite
-		sprites[ 0 ].x =   0;
-		sprites[ 0 ].y =   0;
-		sprites[ 0 ].w = 16;
-		sprites[ 0 ].h = 16;
+    //Set top left sprite
+    sprites[0].x = 0;
+    sprites[0].y = 0;
+    sprites[0].w = 16;
+    sprites[0].h = 16;
 
-		//Set top right sprite
-		sprites[ 1 ].x = 16;
-		sprites[ 1 ].y =   0;
-		sprites[ 1 ].w = 16;
-		sprites[ 1 ].h = 16;
-		
-		//Set bottom left sprite
-		sprites[ 2 ].x =   0;
-		sprites[ 2 ].y = 16;
-		sprites[ 2 ].w = 16;
-		sprites[ 2 ].h = 16;
+    //Set top right sprite
+    sprites[1].x = 16;
+    sprites[1].y = 0;
+    sprites[1].w = 16;
+    sprites[1].h = 16;
 
-		//Set bottom right sprite
-		sprites[ 3 ].x = 16;
-		sprites[ 3 ].y = 16;
-		sprites[ 3 ].w = 16;
-		sprites[ 3 ].h = 16;
-	}
+    //Set bottom left sprite
+    sprites[2].x = 0;
+    sprites[2].y = 16;
+    sprites[2].w = 16;
+    sprites[2].h = 16;
 
-	return success;
+    //Set bottom right sprite
+    sprites[3].x = 16;
+    sprites[3].y = 16;
+    sprites[3].w = 16;
+    sprites[3].h = 16;
 }
 
 void close()
 {
-	//Free loaded images
-	freeSheet();
+    //Free loaded images
+    freeSheet();
 
-	//Destroy window	
-	SDL_DestroyRenderer( gRenderer );
-	SDL_DestroyWindow( gWindow );
-	gWindow = NULL;
-	gRenderer = NULL;
+    //Destroy window	
+    SDL_DestroyRenderer(gRenderer);
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+    gRenderer = NULL;
 
-	//Quit SDL subsystems
-	IMG_Quit();
-	SDL_Quit();
+    //Quit SDL subsystems
+    IMG_Quit();
+    SDL_Quit();
 }
 
 int main( int argc, char* args[] )
 {
-	//Start up SDL and create window
-	if( !init() )
-	{
-		printf( "Failed to initialize!\n" );
-	}
-	else
-	{
-		//Load media
-		if( !loadMedia() )
-		{
-			printf( "Failed to load media!\n" );
-		}
-		else
-		{	
-			//Main loop flag
-			bool quit = false;
+    //Start up SDL and create window
+    init();
+    loadMedia();
+    //Main loop flag
+    bool quit = false;
 
-			//Event handler
-			SDL_Event e;
+    //Event handler
+    SDL_Event e;
 
-			//While application is running
-			while( !quit )
-			{
-				//Handle events on queue
-				while( SDL_PollEvent( &e ) != 0 )
-				{
-					//User requests quit
-					if( e.type == SDL_QUIT )
-					{
-						quit = true;
-					}
-				}
+    //While application is running
+    while( !quit )
+    {
+        //Handle events on queue
+        while( SDL_PollEvent( &e ) != 0 )
+        {
+            //User requests quit
+            if( e.type == SDL_QUIT )
+            {
+                quit = true;
+            }
+        }
 
-				//Clear screen
-				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-				SDL_RenderClear( gRenderer );
+	//Clear screen
+	SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+	SDL_RenderClear( gRenderer );
 
-				//Render top left sprite
-				renderSprite( 0, 0, &sprites[ 0 ] );
+	//Render top left sprite
+	renderSprite( 0, 0, &sprites[ 0 ] );
 
-				//Update screen
-				SDL_RenderPresent( gRenderer );
-			}
-		}
-	}
+	//Update screen
+	SDL_RenderPresent( gRenderer );
+    }
 
-	//Free resources and close SDL
-	close();
+    //Free resources and close SDL
+    close();
 
-	return 0;
+    return 0;
 }
